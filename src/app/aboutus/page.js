29da252e672892d6893b9192e50213fc0bb5f02b
codebyayush <ayushpal6939@gmail.com>
2024-04-28 23:11:31 +0530

    initial commit

---
+++ src/app/aboutus/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center p-24'>About us</div>
+  )
+}
+
+export default page;
---
+++ src/app/home/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='p-24 flex justify-center'>home</div>
+  )
+}
+
+export default home;
