70e4eb74fbaea8a75ce44c1029cf54f088940d69
codebyayush <ayushpal6939@gmail.com>
2024-04-29 11:07:54 +0530

    final touch aboutus

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -2,22 +2,25 @@ import React from "react";
 import Link from "next/link";
 
 const page = () => {
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
 
   return (
     <>
       <div className="flex justify-center p-2">About us</div>
-          <div className="p-10 text-center">
-              <Link href={"/aboutus/1"}>
-                  user 1
-              </Link><br />
-              <Link href={"/aboutus/2"}>
-                  user 2
-              </Link><br />
-              <Link href={"/aboutus/3"}>
-                  user 3
-              </Link><br />
-          </div>
-      
+      <div className="p-10 text-center">
+        {details.map((users) => (
+          <>
+            <Link href={`/aboutus/${users.id}`}>{users.name}</Link>
+            <br />
+            <br />
+          </>
+        ))}
+        <br />
+      </div>
     </>
   );
 };
